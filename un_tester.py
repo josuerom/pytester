a5a4575ba602a616f6a3cfd39cd0686c332841d2
Josué Romero <79048209+josuerom@users.noreply.github.com>
2024-04-27 13:00:42 -0500

    En Unix

--- un_tester.py
+++ un_tester.py
@@ -3,7 +3,6 @@
    created: 25/04/24 10:45:39
 """
 import os
-import re
 import sys
 import shutil
 import subprocess
@@ -106,8 +105,8 @@ def parsear_html(html_content):
 def formatear_captura(captura):
    captura = captura.strip().split("\n")
    limpieza = []
-   for i in captura:
-      if i != "Input" and i != "Output" and i != "":
+   for i in range(len(captura) - 1):
+      if captura[i] != "Input" and captura[i] != "Output" and captura[i] != "" and i < 3:
          limpieza.append(i)
    return '\n'.join(limpieza)
 
@@ -120,7 +119,7 @@ def ejecutar_python(programa):
          break
       with open(entrada_estandar, "r") as contenido_archivo_entrada:
          input_txt = "".join(contenido_archivo_entrada.readlines())
-      proceso = subprocess.Popen(["python3", programa], stdin=subprocess.PIPE, stdout=subprocess.PIPE, 
+      proceso = subprocess.Popen(["python3", "-OO", "-S", "-B", programa], stdin=subprocess.PIPE, stdout=subprocess.PIPE, 
                                  stderr=subprocess.PIPE, text=True)
       salida_generada, _ = proceso.communicate(input=input_txt)
       with open(salida_estandar, "r") as contenido_archivo_respuesta:
